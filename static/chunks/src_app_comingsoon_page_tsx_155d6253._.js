7c740b92c29fd482f2b6e870403ab28920689a1d
Nathan Tr <nathantr@Nathans-MacBook-Pro.local>
2025-05-17 23:07:53 +0700

    Updates

---
+++ static/chunks/src_app_comingsoon_page_tsx_155d6253._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_6021b2a5._.js",
+  "static/chunks/src_e845b1b4._.js",
+  "static/chunks/src_app_comingsoon_styles_34f9233d.css"
+],
+    source: "dynamic"
+});
